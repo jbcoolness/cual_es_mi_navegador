e5bb1e1bb3df63c5343771f82d41c62e63ed71fb
Julio Bertty <julio.bertty@gmail.com>
2019-03-28 11:06:49 -0500

    request desde la consola

--- solution.rb
+++ solution.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
+require 'net/http'
 
 get '/'  do
-    "<h1> #{request.user_agent} </h1>"
+    #"<h1> #{request.user_agent} </h1>"
+    env['PERMISO'] == 'soy-un-token-secreto' ? "Si lo logramos!" : "Sin Permiso"
     
 end

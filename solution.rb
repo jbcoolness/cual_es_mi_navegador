f4ba7fe5dff97f045fa1f8f1d1ac3d9349db1806
Julio Bertty <julio.bertty@gmail.com>
2019-03-28 11:53:32 -0500

    vamos por esta final sinatra2

--- solution.rb
+++ solution.rb
@@ -3,6 +3,6 @@ require 'net/http'
 
 get '/'  do
     #"<h1> #{request.user_agent} </h1>"
-    request["permiso"] == 'soy-un-token-secreto' ? "Si lo logramos!" : "Sin Permiso"
+    request["PERMISO"] == 'soy-un-token-secreto' ? "Si lo logramos!" : "Sin Permiso"
     
 end
